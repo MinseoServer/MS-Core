bef6dde9a84802d8bdb9945f4c8029d8940e16ec
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2023-04-02 10:03:51 +0900

    1.0.8

--- src/main/java/kr/ms/core/extension/JavaPluginExtension.kt
+++ src/main/java/kr/ms/core/extension/JavaPluginExtension.kt
@@ -1,25 +1,23 @@
 package kr.ms.core.extension
 
+import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
-import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-val lazyListener by lazy { object: Listener {} }
-
-inline fun <reified E: Event> JavaPlugin.listen(priority: EventPriority = EventPriority.NORMAL, crossinline event: E.()->Unit) {
-    server.pluginManager.registerEvent(E::class.java,lazyListener, priority, { _, e ->
-        event.invoke(e as E)
+inline fun <reified E: Event> JavaPlugin.listen(priority: EventPriority = EventPriority.NORMAL, crossinline event: E.(listener: Listener)->Unit) {
+    val listener = object: Listener {}
+    server.pluginManager.registerEvent(E::class.java, listener, priority, { _, e ->
+        event.invoke(e as E, listener)
     }, this)
 }
 
-inline fun <reified E: Event> JavaPlugin.listen(priority: EventPriority = EventPriority.NORMAL, crossinline event: E.()->Boolean) {
-    server.pluginManager.registerEvent(E::class.java,lazyListener, priority, { listener, e ->
-        if(event.invoke(e as E)) {
-            val handlerListMethod = E::class.java.getMethod("getHandlerList")
-            val handlerList = handlerListMethod.invoke(null) as HandlerList
-            handlerList.unregister(listener)
-        }
+inline fun <reified E: PlayerEvent> JavaPlugin.listen(priority: EventPriority = EventPriority.NORMAL, target: Player, crossinline event: E.(listener: Listener)->Unit) {
+    val listener = object: Listener {}
+    server.pluginManager.registerEvent(E::class.java, object: Listener {}, priority, { _, e ->
+        e as E
+        if(e.player.uniqueId == target.uniqueId) event.invoke(e, listener)
     }, this)
 }
